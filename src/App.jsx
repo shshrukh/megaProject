af3c97bbe50402483b4d29c9769914c2bf5c7c22
shshrukh <shshrukh.arbaz@gmail.com>
2025-10-15 15:18:42 +0500

    Refactor App component to use appWriteAuth for user authentication and clean up imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,28 @@
-import  { useEffect, useState } from 'react'
-import { useDispatch } from 'react-redux';
-import authServise from './appWrite/auth';
+import { useState, useEffect } from 'react'
+import {useDispatch} from 'react-redux'
+import appWriteAuth from './appWrite/auth'
 import {login, logout} from './feactres/authSlice'
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
- 
-function App() {
- const [loading, setLoading] = useState(true);
- const dispatch = useDispatch();
+import { Flag } from 'appwrite'
 
- useEffect(() => {
-  authServise.getCurruntAccout()
-    .then((data)=>{
-      if(!data) return dispatch(logout());
-      dispatch(login({data}))
-    })
-    .finally(()=> setLoading(false))
- },[])
-  
- return !loading ? (
-  <div className='min-h-screen flex flex-wrap'>
-    <div className='w-full block'>
-      <Header/>
-      <main>
-        {/* Outlet  */}
-      </main>
-      <Footer/>
+const App = () => {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  useEffect(()=>{
+    appWriteAuth.getCurrentUser()
+      .then((userData)=>{
+        if(userData) {
+          login({userData})
+        }else{
+          logout()
+        }
+      })
+      .finally(()=>{setLoading(false)})
+  },[])
+  return (
+    <div>
+      
     </div>
-  </div>
- ):(null)
+  )
 }
 
 export default App
